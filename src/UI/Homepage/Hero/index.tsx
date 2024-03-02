fe75cc6d98cd383d071be9a049d750eee57d62e1
shubham-tam <tamrakarshubham23@gmail.com>
2024-03-02 16:12:04 +0545

    feat: hero section completed

--- src/UI/Homepage/Hero/index.tsx
+++ src/UI/Homepage/Hero/index.tsx
@@ -1,10 +1,12 @@
 import './index.scss';
 import HouseHeroImg from '@/assets/image/house_hero.avif';
+import HandShakeImg from '@/assets/svg/handshake-svgrepo-com.svg';
 
 export const Hero = () => {
     return (
         <div className="home_hero">
             <LeftHeroSection />
+            <SearchBbox />
             <RightHeroSection />
         </div>
     );
@@ -20,7 +22,22 @@ export const RightHeroSection = () => {
             <img src={HouseHeroImg} alt="House Hero" className="home_hero__right__img" />
 
             <div className="home_hero__right__message">
-                <div>left</div>
+                <div className="home_hero__right__message__left">
+                    <div className="home_hero__right__message__left__service_box">
+                        <div className="home_hero__right__message__left__service_box__msg-pitch">
+                            <img src={HandShakeImg} alt="Hand Shake" className="hand-shake" />
+                            <p className="service_pitch">
+                                Lorem Ipsum{' '}
+                                <strong className="service_pitch__strong">Service</strong>
+                            </p>
+                        </div>
+                        <p className="service_message">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                    <button>Learn more</button>
+                </div>
                 <div className="home_hero__right__message__right">
                     <span>House listings</span>
                     <p>
@@ -31,6 +48,19 @@ export const RightHeroSection = () => {
                     </p>
                 </div>
             </div>
+        </div>
+    );
+};
+
+export const SearchBbox = () => {
+    return (
+        <div className="home_hero__searchbox">
+            <p className="home_hero__searchbox__heading">Lorem ipsum dolor</p>
+            <input
+                type="text"
+                placeholder="Search any location..."
+                className="home_hero__searchbox__input"
+            />
         </div>
     );
 };
